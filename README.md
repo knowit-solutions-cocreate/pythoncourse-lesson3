f17fd7a528b8b234bb60fe139e660c319453f2ee
Edvard Lindelöf <edvard.lindelof@knowit.se>
2024-06-25 16:36:34 +0200

    move in subset from lesson 2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code for Python course lesson 3
+Welcome to part three of the Python course!
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='lesson_3_code',
+    version='0.1.0',
+    python_requires='>=3.12',
+    packages=['lesson_3_code'],
+    install_requires=[
+        'setuptools >= 68.2.2, < 69',
+        'sqlalchemy >= 2.0.30, < 2.1',
+        'pandas >= 2.2.2, < 2.3',
+        'requests >= 2.32.3',
+    ],
+)
